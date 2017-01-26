224a913061db25c1ff9f5e5ed9402a232d055317
Jim Kjelland <Jim Kjelland>
2017-01-25 18:39:04 -0800

    attempt to fix

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
+// Add your requirements
 var restify = require('restify');
 var builder = require('botbuilder');
 
-//=========================================================
-// Bot Setup
-//=========================================================
-
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
+server.listen(process.env.PORT || 3000, function()
+{
    console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat bot
-var connector = new builder.ChatConnector({
-    appId: 9590c808-42d9-44a0-adb8-037f3f628a82,
-    appPassword: WkvV238tnNkNbkEfSnYr9Tx
-});
+var connector = new builder.ChatConnector
+({ appId: 'YourAppId', appPassword: 'YourAppPassword' });
 var bot = new builder.UniversalBot(connector);
-server.post('/API/Messages', connector.listen());
+server.post('/api/messages', connector.listen());
 
-//=========================================================
-// Bots Dialogs
-//=========================================================
-
+// Create bot dialogs
 bot.dialog('/', function (session) {
     session.send("Hello World");
 });
