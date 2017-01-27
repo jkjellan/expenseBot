5ba195283105f7430c26c08f401f6eefa4fc7f0f
Jim Kjelland <Jim Kjelland>
2017-01-26 20:21:35 -0800

    added expense v. headcount dialog logic

--- app.js
+++ app.js
@@ -20,9 +20,38 @@ var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 // Create bot dialogs
-bot.dialog('/', function (session) {
-    session.send("Hello Worlds");
-});
+bot.dialog('/',[
+    function(session, args,next){
+        if(session.userData.qtype != 'expense' || session.userData.qtype != 'headcount'){
+            session.beginDialog('/qtype');
+        } else{
+            next();
+        }
+    },
+    function(session,results){
+    if(session.userData.qtype == 'expense' || session.userData.qtype == 'headcount' ){
+        session.send("I can definitely answer your questions about %s",session.userData.qtype);
+    }
+    else {
+        session.send("I don't know anything about %s",session.userData.qtype);
+        session.beginDialog('/');
+    }
+}
+]);
+
+bot.dialog('/qtype', [
+    
+    function (session) {
+        builder.Prompts.text(session,"Hi, Do you want to know about expense or headcount?");
+    },
+    function(session,results){
+        
+            session.userData.qtype = results.response;
+            session.endDialog();
+        
+    }
+]);
+
 
 server.get('/', restify.serveStatic({
  directory: __dirname,
