84f141836fc915168f81831dd89a890253629ed9
Jim Kjelland <Jim Kjelland>
2017-01-26 18:29:51 -0800

    1

--- app.js
+++ app.js
@@ -4,23 +4,28 @@ var builder = require('botbuilder');
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.PORT || 3000, function()
+server.listen(process.env.PORT || 3978, function()
 {
    console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat bot
-var connector = new builder.ChatConnector
-({ appId: process.env.MY_APP_ID, appPassword: process.env.MY_APP_PASSWORD});
+//var connector = new builder.ChatConnector
+//({ appId: process.env.MY_APP_ID, appPassword: process.env.MY_APP_PASSWORD});
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
 var bot = new builder.UniversalBot(connector);
-server.post('/API/Messages', connector.listen());
+server.post('/api/messages', connector.listen());
 
 // Create bot dialogs
 bot.dialog('/', function (session) {
-    session.send("Hello World");
+    session.send("Hello Worlds");
 });
 
-server.get('/', restify.serveStatic({
- directory: __dirname,
- default: '/index.html'
-}));
+// server.get('/', restify.serveStatic({
+//  directory: __dirname,
+//  default: '/index.html'
+// }));
