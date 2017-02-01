ddaf9739192ead9fb7f9e39ed60eac600e699da1
jkjellan <jkjellan@gmail.com>
2017-02-01 09:52:28 -0800

    merging changes from work computer

--- consoleBot.js
+++ consoleBot.js
@@ -15,6 +15,34 @@ var connector = new builder.ConsoleConnector().listen();
 var bot = new builder.UniversalBot(connector);
 
 // Create bot dialogs
-bot.dialog('/', function (session) {
-   session.send('Hi My Name is Jim.  I am the number 3.');
-});
+bot.dialog('/',[
+    function(session, args,next){
+        if(session.userData.qtype != 'expense' || session.userData.qtype != 'headcount'){
+            session.beginDialog('/qtype');
+        } else{
+            next();
+        }
+    },
+    function(session,results){
+    if(session.userData.qtype == 'expense' || session.userData.qtype == 'headcount' ){
+        session.send("I can definitely answer your questions about %s",session.userData.qtype);
+    }
+    else {
+        session.send("I don't know anything about %s",session.userData.qtype);
+        session.beginDialog('/');
+    }
+}
+]);
+
+bot.dialog('/qtype', [
+    
+    function (session) {
+        builder.Prompts.text(session,"Hi, Do you want to know about expense or headcount?");
+    },
+    function(session,results){
+        
+            session.userData.qtype = results.response;
+            session.endDialog();
+        
+    }
+]);
