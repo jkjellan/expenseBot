d32787fb4e04f17c68e14eda2be37f9c8c58b2d5
Jim Kjelland <Jim Kjelland>
2017-02-16 13:58:57 -0800

    removed LUIS

--- app.js
+++ app.js
@@ -16,12 +16,12 @@ var connector = new builder.ChatConnector({
     appPassword: process.env.MY_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
-server.post('https://luis-actions.cloudapp.net/api/v1/botframework?app-id=0788ebe8-b8f3-4748-b59f-3d3298aae151&subscription-key=b7831c85b83244b5a42356a1c6374da4/api/messages', connector.listen());
+server.post('/api/messages', connector.listen());
 
-var LUISmodelURL = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/0788ebe8-b8f3-4748-b59f-3d3298aae151?subscription-key=b7831c85b83244b5a42356a1c6374da4';
-var recognizer = new builder.LuisRecognizer(LUISmodelURL);
-var intents = new builder.IntentDialog({ recognizers: [recognizer] });
-bot.dialog('/', intents);
+// var LUISmodelURL = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/0788ebe8-b8f3-4748-b59f-3d3298aae151?subscription-key=b7831c85b83244b5a42356a1c6374da4';
+// var recognizer = new builder.LuisRecognizer(LUISmodelURL);
+// var intents = new builder.IntentDialog({ recognizers: [recognizer] });
+// bot.dialog('/', intents);
 
 //connect to database
 var Connection = tedious.Connection;  
@@ -52,8 +52,7 @@ var Connection = tedious.Connection;
         request = new Request(sqlQuery, function(err) {  
         if (err) {  
             console.log('I is error');}  
-        });  
-      
+        });     
         request.on('row', function(columns) {  
             columns.forEach(function(column) {  
               if (column.value === null) {  
@@ -66,85 +65,84 @@ var Connection = tedious.Connection;
             //console.log(result);  
             //result ="";
             callback(); 
-        });  
-  
+        });   
         request.on('done', function(rowCount, more) {  
         console.log(rowCount + ' rows returned');  
         });  
         connection.execSql(request);  
     }  
 
-intents.matches('budget',[
-    function (session, args, next) {
-        session.send('Welcome to the expense Bot!');
-    }]);
+// intents.matches('budget',[
+//     function (session, args, next) {
+//         session.send('Welcome to the expense Bot!');
+//     }]);
 
-intents.onDefault(builder.DialogAction.send("whatchyoutalkinbout"));
+// intents.onDefault(builder.DialogAction.send("whatchyoutalkinbout"));
 
-// Create bot dialogs
-// bot.dialog('/',[
-//     function(session, args,next){
-//         session.beginDialog('/intro');
-//     }
-// ]);
+Create bot dialogs
+bot.dialog('/',[
+    function(session, args,next){
+        session.beginDialog('/intro');
+    }
+]);
 
-// bot.dialog('/intro', [
+bot.dialog('/intro', [
     
-//     function (session) {
-//         builder.Prompts.text(session,"Well hello there. How are you doing?");
-//     },
-//     function(session,results){
-//         session.userData.doingWell = results.response;
-//         session.send("That's nice...");       
-//         builder.Prompts.text(session, "Would you like to see a magic trick?");
-//     },
-//     function(session,results){
-//         session.userData.magic = results.response;
-//         if(session.userData.magic == 'yes' || session.userData.magic == 'Yes'){
-//             session.beginDialog('/magic');
-//         }
-//         else if(session.userData.magic == 'no' || session.userData.magic == 'No' ){
-//             session.send("I guess this is goodbye, then");
-//         }
-//         else{
-//             session.send("It's a simple yes or no question");
-//         }   
-//     }
-// ]);
+    function (session) {
+        builder.Prompts.text(session,"Well hello there. How are you doing?");
+    },
+    function(session,results){
+        session.userData.doingWell = results.response;
+        session.send("That's nice...");       
+        builder.Prompts.text(session, "Would you like to see a magic trick?");
+    },
+    function(session,results){
+        session.userData.magic = results.response;
+        if(session.userData.magic == 'yes' || session.userData.magic == 'Yes'){
+            session.beginDialog('/magic');
+        }
+        else if(session.userData.magic == 'no' || session.userData.magic == 'No' ){
+            session.send("I guess this is goodbye, then");
+        }
+        else{
+            session.send("It's a simple yes or no question");
+        }   
+    }
+]);
 
-// bot.dialog('/magic',[
-//     function(session){
-//         builder.Prompts.text(session, "Ok, give me the name of an Agency Insights department, such as Channel Insights");
-//     },
-//     function(session,results){
-//         deptVar = results.response;
-//         builder.Prompts.text(session,"Now give me the name of an expense category, such as Travel");
-//     },
-//     function(session,results){
-//         acctVar = results.response;
-//         result = "";
-//         executeStatement(function(){
-//             session.beginDialog('/answer');
-//         });       
-//         }
-//     ]);
+bot.dialog('/magic',[
+    function(session){
+        builder.Prompts.text(session, "Ok, give me the name of an Agency Insights department, such as Channel Insights");
+    },
+    function(session,results){
+        deptVar = results.response;
+        builder.Prompts.text(session,"Now give me the name of an expense category, such as Travel");
+    },
+    function(session,results){
+        acctVar = results.response;
+        result = "";
+        executeStatement(function(){
+            session.beginDialog('/answer');
+        });       
+        }
+    ]);
 
-// bot.dialog('/answer',[
-//     function(session){
-//         session.send("The %s budget for %s in 2017 is %s",acctVar,deptVar,result);
-//         builder.Prompts.text(session,"Would you like to see another trick?");
-//     },
-//     function(session,results){
-//         if(results.response == 'yes' || results.response == 'Yes'){
-//             session.beginDialog('/magic');
-//         }
-//         else if(results.response == 'no' || results.response == 'No'){
-//             session.endDialog("Goodbye");
-//         }
-//         else {
-//             session.send("It was a simple yes or no question");
-//             session.endDialog("See ya");
-//         }
+bot.dialog('/answer',[
+    function(session){
+        session.send("The %s budget for %s in 2017 is %s",acctVar,deptVar,result);
+        builder.Prompts.text(session,"Would you like to see another trick?");
+    },
+    function(session,results){
+        if(results.response == 'yes' || results.response == 'Yes'){
+            session.beginDialog('/magic');
+        }
+        else if(results.response == 'no' || results.response == 'No'){
+            session.endDialog("Goodbye");
+        }
+        else {
+            session.send("It was a simple yes or no question");
+            session.endDialog("See ya");
+        }
 
-//     }
-// ]);
+    }
+]);
