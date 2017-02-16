1aadc9e259949463c320ca265507d6f86edf19ac
Jim Kjelland <Jim Kjelland>
2017-02-16 14:08:33 -0800

    test

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ var Connection = tedious.Connection;
 // intents.onDefault(builder.DialogAction.send("whatchyoutalkinbout"));
 
 //Create bot dialogs
-bot.dialog('/',[
+bot.dialog('/api/messages',[
     function(session, args,next){
         session.beginDialog('/intro');
     }
@@ -146,3 +146,4 @@ bot.dialog('/answer',[
 
     }
 ]);
+
