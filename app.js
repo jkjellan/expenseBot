9ba60904a63803ff7f8ec4d21a312ab21a514d58
jkjellan <jkjellan@gmail.com>
2017-02-22 15:40:37 -0800

    new database on enterprise account

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ var Connection = tedious.Connection;
         password: 'DIbotadmin1',
         server: 'distributioninsightsserver.database.windows.net',  
         // If you are on Microsoft Azure, you need this:  
-        options: {encrypt: true, database: 'expensesbotdb'}  
+        options: {encrypt: true, database: 'DistributionInsightsDB'}  
     };  
     var connection = new Connection(config);  
     connection.on('connect', function(err) {  
